869f2968cfd4cae2efeeda7ae3e6cc9120734bd8
devenmithal <devenmithal@gmail.com>
2019-06-10 23:14:52 -0500

    Just finished isAtEnd method

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -29,8 +29,85 @@ class Scanner {
 	    return tokens;                                  
 	  }
   
+  private void scanToken() {                     
+	    char c = advance();                          
+	    switch (c) {                                 
+	      case '(': addToken(LEFT_PAREN); break;   
+	      
+	      case ')': addToken(RIGHT_PAREN); break; 
+	      
+	      case '{': addToken(LEFT_BRACE); break;
+	      
+	      case '}': addToken(RIGHT_BRACE); break; 
+	      
+	      case ',': addToken(COMMA); break;     
+	      
+	      case '.': addToken(DOT); break;   
+	      
+	      case '-': addToken(MINUS); break;   
+	      
+	      case '+': addToken(PLUS); break;       
+	      
+	      case ';': addToken(SEMICOLON); break;      
+	      
+	      case '*': addToken(STAR); break; 
+	      
+	      case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+	      
+	      case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;   
+	      
+	      case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;    
+	      
+	      case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+	      //conditional operator 
+	      //variable x = (expression) ? value if true : value if false
+	      
+	      case '/':                                                       
+	          if (match('/')) {                                             
+	            // A comment goes until the end of the line.                
+	            while (peek() != '\n' && !isAtEnd()) advance();             
+	          } else {                                                      
+	            addToken(SLASH);                                            
+	          }                                                             
+	          break; 
+	          
+	      default:                                     
+	        Lox.error(line, "Unexpected character.");
+	        break; 
+	        //when we detect an error, we never execute the code, but we still do
+	        //continue scanning 
+	    }                                            
+	  }
+  
+  private boolean match(char expected) {                 
+	    if (isAtEnd()) return false;                         
+	    if (source.charAt(current) != expected) return false;
+
+	    current++;                                           
+	    return true;                                         
+	  }
+  private char peek() {           
+	    if (isAtEnd()) return '\0';   
+	    return source.charAt(current);
+	  } 
+  
   private boolean isAtEnd() {         
 	    return current >= source.length();
-	  } 
+	  }
+  //Last method added 10/06/19
+  
+  private char advance() {                               
+	    current++;                                           
+	    return source.charAt(current - 1);                   
+	  }
+
+	  private void addToken(TokenType type) {                
+	    addToken(type, null);                                
+	  }                                                      
+
+	  private void addToken(TokenType type, Object literal) {
+	    String text = source.substring(start, current);      
+	    tokens.add(new Token(type, text, literal, line));    
+	  }  
   
 } 
