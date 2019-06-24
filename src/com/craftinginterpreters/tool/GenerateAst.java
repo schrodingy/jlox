2e95759b9bb90af104fa8bb1cd2e8c0d8d4e6404
devenmithal <devenmithal@gmail.com>
2019-06-23 23:47:30 -0500

    Expr class redone

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -5,21 +5,22 @@ import java.io.PrintWriter;
 import java.util.Arrays;                                           
 import java.util.List;                                             
 
+// pwd: /Users/devenmithal/Documents/workspace/com.craftinginterpreters.lox/src/com/craftinginterpreters/tool
+
 public class GenerateAst {                                         
-  public static void main(String[] args) throws IOException {      
-    if (args.length != 1) {                                        
-      System.err.println("Usage: generate_ast <output directory>");
-      System.exit(1);                                              
-    }                                                              
-    String outputDir = args[0];   
-    defineAst(outputDir, "Expr", Arrays.asList(          
-    	      "Binary   : Expr left, Token operator, Expr right",
-    	      "Grouping : Expr expression",                      
-    	      "Literal  : Object value",                         
-    	      "Unary    : Token operator, Expr right"            
-    	    ));
-  }
-  
+	 public static void main(String[] args) throws IOException {      
+		    if (args.length != 1) {                                        
+		      System.err.println("Usage: generate_ast <output directory>");
+		      System.exit(1);                                              
+		    }                                                              
+		    String outputDir = args[0];   
+		    defineAst(outputDir, "Expr", Arrays.asList(          
+		    	      "Binary   : Expr left, Token operator, Expr right",
+		    	      "Grouping : Expr expression",                      
+		    	      "Literal  : Object value",                         
+		    	      "Unary    : Token operator, Expr right"            
+		    	    ));
+		  }  
   private static void defineAst(                            
 	      String outputDir, String baseName, List<String> types)
 	      throws IOException {                                  
