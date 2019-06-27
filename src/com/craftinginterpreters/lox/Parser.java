9645bf9a7e053e9ea72773ca6a8e8db50f980440
devenmithal <devenmithal@gmail.com>
2019-06-26 23:49:07 -0500

    Continue developing parser

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -10,5 +10,21 @@ class Parser {
 
   Parser(List<Token> tokens) {                         
     this.tokens = tokens;                              
-  }                                                    
+  }       
+  
+  private Expr expression() {
+	    return equality();       
+	  } 
+  
+  private Expr equality() {                         
+	    Expr expr = comparison();
+
+	    while (match(BANG_EQUAL, EQUAL_EQUAL)) {        
+	      Token operator = previous();                  
+	      Expr right = comparison();                    
+	      expr = new Expr.Binary(expr, operator, right);
+	    }                                               
+
+	    return expr;                                    
+	  } 
 } 
